0c4bf6b64f778cf386841d0215d1730acd80916c
AKASH-kce <127519370+AKASH-kce@users.noreply.github.com>
2025-08-11 11:22:07 +0530

    router setup

--- BondTalechatClient/src/app/app.routes.ts
+++ BondTalechatClient/src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:LoginComponent},
+    {path:'signUp',component:SignupComponent},
+    {path:'forgotPassword',component:ForgotPasswordComponent}
+];
