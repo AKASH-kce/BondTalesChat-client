f4a6c773f79e618999900b36756babefded9e279
AKASH-kce <127519370+AKASH-kce@users.noreply.github.com>
2025-08-12 21:54:56 +0530

    user list add

--- BondTalechatClient/src/app/components/users-list-side-bar/users-list-side-bar.component.ts
+++ BondTalechatClient/src/app/components/users-list-side-bar/users-list-side-bar.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+export interface IUserMessage {
+  ProfileImageURl: string;
+  name: string;
+  lastMessageTime: string;
+  lastMessage: string;
+}
 @Component({
   selector: 'app-users-list-side-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users-list-side-bar.component.html',
   styleUrl: './users-list-side-bar.component.scss'
 })
-export class UsersListSideBarComponent {
+export class UsersListSideBarComponent implements OnInit {
+
+  ProfileImageURl: string = "/images/profile.jpeg"
+  users: IUserMessage[] = [];
+
+  ngOnInit(): void {
+    this.users.push(
+      {
+        name: "akash",
+        ProfileImageURl: this.ProfileImageURl,
+        lastMessageTime: "time",
+        lastMessage: "last message"
+      },
+      {
+        name: "koushiik",
+        ProfileImageURl: this.ProfileImageURl,
+        lastMessageTime: "time",
+        lastMessage: "last message"
+      }
+    )
+  }
 
 }
