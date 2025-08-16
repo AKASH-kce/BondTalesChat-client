5a83de3e37eecc2b008108c4ec70bd9fb298a110
AKASH-kce <127519370+AKASH-kce@users.noreply.github.com>
2025-08-16 18:02:41 +0530

    chathub update

--- BondTalechatClient/src/app/components/previous-chat-list-area/previous-chat-list-area.component.ts
+++ BondTalechatClient/src/app/components/previous-chat-list-area/previous-chat-list-area.component.ts
@@ -7,33 +7,34 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-previous-chat-list-area',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './previous-chat-list-area.component.html',
-  styleUrl: './previous-chat-list-area.component.scss'
+  styleUrls: ['./previous-chat-list-area.component.scss']
 })
 export class PreviousChatListAreaComponent implements OnInit, OnDestroy {
-  constructor(private chatService: ChatService) {
-
-  }
   messages: any[] = [];
+  isTyping: boolean = false;
+  ProfileImageURl: string = "/images/profile.jpeg";
   private messageSub?: Subscription;
 
-async ngOnInit(): Promise<void> {
+  constructor(private chatService: ChatService) {}
+
+  async ngOnInit(): Promise<void> {
     await this.chatService.startConnection(); // wait until connected
 
     // Fetch all messages after connection is established
     this.messages = await this.chatService.getAllMessageOfCurrentLoginUser(1);
     console.log('All messages:', this.messages);
 
+    // Subscribe to incoming messages
     this.messageSub = this.chatService.message$.subscribe((msg: any) => {
-        if (msg) {
-            this.messages.push(msg);
-        }
+      if (msg && !this.messages.some(m => m.messageId === msg.messageId)) {
+        this.messages.push(msg);
+      }
     });
-}
+  }
 
   ngOnDestroy(): void {
     this.messageSub?.unsubscribe();
   }
-
 }
