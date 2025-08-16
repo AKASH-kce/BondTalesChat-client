f3006db4a0c70c946fca8f80be013ebad7457d4b
AKASH-kce <127519370+AKASH-kce@users.noreply.github.com>
2025-08-16 15:31:17 +0530

    messages display functionality

--- BondTalechatClient/src/app/components/previous-chat-list-area/previous-chat-list-area.component.ts
+++ BondTalechatClient/src/app/components/previous-chat-list-area/previous-chat-list-area.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChatService } from '../../services/chat.serivce';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-previous-chat-list-area',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './previous-chat-list-area.component.html',
   styleUrl: './previous-chat-list-area.component.scss'
 })
-export class PreviousChatListAreaComponent {
+export class PreviousChatListAreaComponent implements OnInit, OnDestroy {
+  constructor(private chatService: ChatService) {
+
+  }
+  messages: any[] = [];
+  private messageSub?: Subscription;
+  ngOnInit(): void {
+    this.messageSub = this.chatService.message$.subscribe((msg: any) => {
+      if (msg) {
+        this.messages.push(msg);
+      }
+    })
+  }
+  ngOnDestroy(): void {
+    this.messageSub?.unsubscribe();
+  }
 
 }
